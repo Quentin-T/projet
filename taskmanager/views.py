ed47a8c1d4ceabecc10447e313e664ef72f815bb
Quentin-T <quentin.thibaud@student.isae-supaero.fr>
2020-05-05 18:18:10 +0200

    Modification de l'apparence des formulaires de journal et de nouvelle tache avec dans les templates, ajout de quelques commentaires

--- taskmanager/views.py
+++ taskmanager/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from .models import Project, Task, Status, Journal
 from .forms import JournalForm, TaskForm
@@ -28,10 +29,12 @@ def task(request, task_id):
 
 @login_required
 def newjournal(request, task_id):
+    "ajout d'une nouvelle entrée de journal"
     task = Task.objects.get(id=task_id)
     form = JournalForm(request.POST or None)
 
     if form.is_valid():
+        "on attribue par défaut l'utilisateur qui ajoute le journal et la date de publication"
         journal = Journal()
         journal.author = request.user
         journal.entry = form.cleaned_data["entry"]
@@ -44,16 +47,23 @@ def newjournal(request, task_id):
 @login_required
 def newtask(request, proj_id):
     form = TaskForm(request.POST or None)
+
+    """on récupère le projet en cours"""
     project = Project.objects.get(id=proj_id)
+
+    """on recupère les utilisateurs et status pour les sélectionner dans le formulaire"""
+    status = Status.objects.all()
+    users = User.objects.all()
+
     if form.is_valid():
-        task = Task(projet)
-        task.name = form.cleaned_data['name']
-        task.assignee = form.cleaned_data['assignee']
-        task.description = form.cleaned_data['description']
-        task.start_date = form.cleaned_data['start_date']
-        task.due_date = form.cleaned_data['due_date']
-        task.priority = form.cleaned_data['priority']
-        task.status = form.cleaned_data['status']
+        task = Task(projet=project)
+        task.name = form.cleaned_data["name"]
+        task.assignee = form.cleaned_data["assignee"]
+        task.description = form.cleaned_data["description"]
+        task.start_date = form.cleaned_data["start_date"]
+        task.due_date = form.cleaned_data["due_date"]
+        task.priority = form.cleaned_data["priority"]
+        task.status = form.cleaned_data["status"]
         task.save()
         return redirect("task", task.id)
     return render(request, "newtask.html", locals())
@@ -62,6 +72,7 @@ def newtask(request, proj_id):
 @login_required
 def edittask(request, task_id):
     task = Task.objects.get(id=task_id)
+    """on initialise le formulaire avec le contenu de task"""
     form = TaskForm(request.POST or None, instance=task)
 
     if form.is_valid():
